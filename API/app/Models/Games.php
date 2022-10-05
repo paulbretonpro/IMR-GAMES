d7d5126440c49487792bcac553bbffef66969e6d
paulbreton <paul.breton@cooperl.com>
2022-10-05 19:20:25 +0200

    feat(roles): create table roles & link roles with each games

--- API/app/Models/Games.php
+++ API/app/Models/Games.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Games extends Model
 {
@@ -15,4 +16,12 @@ class Games extends Model
     ];
 
     protected $primaryKey = 'id';
+
+    /**
+     * @return HasMany
+     */
+    public function roles() :HasMany
+    {
+        return $this->hasMany(Roles::class);
+    }
 }
